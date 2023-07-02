9d4b2d291c3ba7df54cd37c6d202edf66daa1774
Priyanka Kashyap <prikashy@publicisgroupe.net>
2023-07-02 13:18:44 +0530

    Hello World java code2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
-public class Hello {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
+import java.util.*;
+public class Java
+{
+public static void main(String args[])
+{
+System.out.println("Hello World");
+}
 }
